392aa54b4f5c2e57b3c0ba93d0f39da182596371
ktx203 <78192053+ktx203@users.noreply.github.com>
2021-11-29 11:01:25 -0500

    Added initial files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
 import './App.css';
 
 function App() {
@@ -7,17 +9,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>snow fuck you</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          
         >
           Learn React
         </a>
       </header>
+      <Switch defaultChecked />
+      <Switch  />
+      <Switch disabled defaultChecked />
+      <Switch disabled />
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
